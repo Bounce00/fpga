112ee20a2c49c574743d7313f1c33109290689cf
zhaijun <Haijun.zhang@sigtrum.com>
2018-09-14 08:16:28 +0800

    keep source files

--- hls/xilinx_design/examples/design/FIR/fir_2ch_int/fir_2ch/solution1/.autopilot/db/fir_top.tbgen.tcl
+++
@@ -1,9 +0,0 @@
-set C_TypeInfoList {{ 
-"fir_top" : [[], { "return": [[], "void"]} , [{"ExternC" : 0}], [ {"din_i": [[], {"array": ["0", [2500]]}] }, {"din_q": [[], {"array": ["0", [2500]]}] }, {"dout_i": [[], {"array": ["1", [2500]]}] }, {"dout_q": [[], {"array": ["1", [2500]]}] }],[],""], 
-"1": [ "dout_t", {"typedef": [[[],"2"],""]}], 
-"2": [ "ap_fixed<40, 9, 5, 3, 0>", {"hls_type": {"ap_fixed": [[[[], {"scalar": { "int": 40}}],[[], {"scalar": { "int": 9}}],[[], {"scalar": { "3": 5}}],[[], {"scalar": { "4": 3}}],[[], {"scalar": { "int": 0}}]],""]}}], 
-"4": [ "ap_o_mode", {"enum": [[],[],[{"SC_SAT":  {"scalar": "__integer__"}},{"SC_SAT_ZERO":  {"scalar": "__integer__"}},{"SC_SAT_SYM":  {"scalar": "__integer__"}},{"SC_WRAP":  {"scalar": "__integer__"}},{"SC_WRAP_SM":  {"scalar": "__integer__"}}],""]}], 
-"3": [ "ap_q_mode", {"enum": [[],[],[{"SC_RND":  {"scalar": "__integer__"}},{"SC_RND_ZERO":  {"scalar": "__integer__"}},{"SC_RND_MIN_INF":  {"scalar": "__integer__"}},{"SC_RND_INF":  {"scalar": "__integer__"}},{"SC_RND_CONV":  {"scalar": "__integer__"}},{"SC_TRN":  {"scalar": "__integer__"}},{"SC_TRN_ZERO":  {"scalar": "__integer__"}}],""]}], 
-"0": [ "din_t", {"typedef": [[[],"5"],""]}], 
-"5": [ "ap_fixed<16, 1, 5, 3, 0>", {"hls_type": {"ap_fixed": [[[[], {"scalar": { "int": 16}}],[[], {"scalar": { "int": 1}}],[[], {"scalar": { "3": 5}}],[[], {"scalar": { "4": 3}}],[[], {"scalar": { "int": 0}}]],""]}}]
-}}
